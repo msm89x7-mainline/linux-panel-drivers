364e0eb36c3974a5483f0be0dc89ed9bec094db0
Barnabás Czémán <trabarni@gmail.com>
2024-08-24 00:49:55 +0200

    config/dtb: xiaomi-santoni: Add panel drivers

---
+++ config/xiaomi-santoni.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+OPTIONS=()
+PANELS=(
+	[hx8394f_boe_720p_video]="xiaomi,santoni-hx8394f-boe"
+	[ili9881c_ebbg_720p_video]="xiaomi,santoni-ili9881c-ebbg"
+	[ili9881c_ebbgdjn_720p_video]="xiaomi,santoni-ili9881c-ebbgdjn"
+)
