285e308fb838433e463a816c914faa692f0d583c
Barnabás Czémán <trabarni@gmail.com>
2024-08-24 11:34:23 +0200

    config/dtb: xiaomi-riva: Add panel drivers

---
+++ config/xiaomi-riva.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+OPTIONS=(-r vsn -r vsp)
+PANELS=(
+	[hx8394f_boe_c3b_720p_video]="xiaomi,riva-hx8394f-boe"
+	[ili9881c_ebbg_c3b_720p_video]="xiaomi,riva-ili9881c-ebbg"
+	[ili9881c_ebbgdjn_c3b_720p_video]="xiaomi,riva-ili9881c-ebbgdjn"
+	[ili9881c_tianma_c3b_720p_video]="xiaomi,riva-ili9881c-tianma"
+)
