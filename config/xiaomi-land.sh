da7d06c67f24d4873a79cb12079d22e54156e3f3
Barnabás Czémán <trabarni@gmail.com>
2024-08-25 21:01:09 +0200

    config/dtb: xiaomi-land: Add panel drivers

---
+++ config/xiaomi-land.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+OPTIONS=()
+PANELS=(
+	[hx8394f_boe_720p_video]="xiaomi,land-hx8394f-boe"
+	[ili9881c_tianma_720p_video]="xiaomi,land-ili9881c-tianma"
+	[nt35521s_ebbg_720p_video]="xiaomi,land-nt35521s-ebbg"
+)
