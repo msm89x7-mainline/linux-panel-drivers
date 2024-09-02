12cf486cad8d407a287b5a9bb072c1c31e81e06e
Barnabás Czémán <trabarni@gmail.com>
2024-09-02 17:24:27 +0200

    config/dtb: xiaomi-ugg/ugglite: add panels

---
+++ config/xiaomi-ugglite.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+OPTIONS=()
+PANELS=(
+	[hx_otm1901a_720p_video]="xiaomi,ugglite-otm1901a-hx"
+	[sc_ili9881c_720p_video]="xiaomi,ugglite-ili9881c-sc"
+	[tm_otm1901a_720p_video]="xiaomi,ugglite-otm1901a-tm"
+)
