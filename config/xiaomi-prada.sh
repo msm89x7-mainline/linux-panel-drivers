632ef1effba02c588e805dfd0d6a5c3513864269
NekoCWD <nekodevelopper@gmail.com>
2025-01-07 18:39:35 +0300

    config/dtb: xiaomi-prada: Add panel drivers

---
+++ config/xiaomi-prada.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+OPTIONS=()
+PANELS=(
+	[hx8394f_boe_720p_video]="xiaomi,prada-hx8394f-boe"
+	[ili9881c_tianma_720p_video]="xiaomi,prada-ili9881c-tianma"
+	[nt35521s_ctc_720p_video]="xiaomi,prada-nt35521s-ctc"
+)
