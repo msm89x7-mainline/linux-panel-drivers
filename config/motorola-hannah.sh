a2a23f568b248f9df65f5c23f9a99463b7a7eb40
Barnabás Czémán <trabarni@gmail.com>
2025-01-12 22:35:20 +0100

    config/dtb: motorola-hannah: add panels

---
+++ config/motorola-hannah.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+OPTIONS=()
+PANELS=(
+	[djn_599_hd_video_v0]="motorola,hannah-599-djn"
+	[djn_600_hd_video_v0]="motorola,hannah-600-djn"
+	[tianma_599_hd_video_v0]="motorola,hannah-599-tianma"
+)
