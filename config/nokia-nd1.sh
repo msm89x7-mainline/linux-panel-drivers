de3d96a57833af8def1e96d347cbd2a3cc536a61
Andrew Gigena <mail@andrewgigena.dev>
2025-07-14 17:27:19 -0300

    config/dtb: nokia-nd1: Add panel drivers

---
+++ config/nokia-nd1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+OPTIONS=()
+PANELS=(
+	[mdss_dsi_pj050ia_nt35521s_720p_video]="nokia,nd1-nt35521s-pj050ia"
+	[mdss_dsi_hx8394a_720p_video]="nokia,nd1-hx8394a"
+	[mdss_dsi_ili7807e_1080p_video]="nokia,nd1-ili7807e"
+	[mdss_dsi_nt35521_720p_video]="nokia,nd1-nt35521"
+	[mdss_dsi_nt35521s_720p_video]="nokia,nd1-nt35521s"
+)
