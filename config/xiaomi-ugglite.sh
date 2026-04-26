c7294934654d16581bb602ec02bf64481795d74e
Barnabás Czémán <trabarni@gmail.com>
2026-04-26 22:27:09 +0200

    config/xiaomi-ugglite: rename the regulators
    Rename the regulators for better HW representation.

--- config/xiaomi-ugglite.sh
+++ config/xiaomi-ugglite.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-OPTIONS=(-r vsn -r vsp --backlight-gpio --dcs-no-get-brightness)
+OPTIONS=(-r vddio -r avee -r avdd --backlight-gpio --dcs-no-get-brightness)
 PANELS=(
 	[hx_otm1901a_720p_video]="xiaomi,ugglite-otm1901a-hx"
 	[sc_ili9881c_720p_video]="xiaomi,ugglite-ili9881c-sc"
