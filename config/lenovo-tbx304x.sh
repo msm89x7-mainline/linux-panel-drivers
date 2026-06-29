ffdbe73bfaf407812afcdb8f97455a20126361b5
pem120 <18i897.iiibb@gmail.com>
2026-06-29 17:10:32 +0530

    config/dtb: lenovo-tbx304L: Add panel drivers

---
+++ config/lenovo-tbx304x.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+OPTIONS=(--regulator power)
+PANELS=(
+	[nt35521s_boe_wxga_video]="lenovo,tbx304x-nt35521s-boe"
+	[nt35521s_wxga_video]="lenovo,tbx304x-nt35521s"
+	[jd9364_boe_video]="lenovo,tbx304x-jd9364-boe"
+)
